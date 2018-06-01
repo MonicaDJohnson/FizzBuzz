947674fcc2db7f99b66e567ad70145a1eb5c6acb
MonicaDJohnson <johnsondmonica290@gmail.com>
2018-05-31 22:39:51 -0400

    homework testing

--- FizzBuzz/FizzBuzzer.cs
+++ FizzBuzz/FizzBuzzer.cs
@@ -4,13 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FizzBuzz
+namespace FizzBuzz//Monica Johnson Homework
 {
     public class FizzBuzzer
     {
         public string Get(int num)
         {
-            return "1";
+
+            if (IsMultipleOf3(num))
+            {
+                return "Fizz";
+            }
+
+            if (IsMultipleOf5(num))
+            {
+                return "Buzz";
+            }
+           
+            if (IsMultipleOf3And5(num)) 
+            {
+                return "FizzBuzz";
+            }
+            return num.ToString();
+            
+
         }
+
+        private bool IsMultipleOf3(int numberToCheck)
+        {
+            return numberToCheck % 3 == 0;
+        }
+
+        private bool IsMultipleOf5(int numberToCheck)
+        {
+            return numberToCheck % 5 == 0;
+        }
+
+        private bool IsMultipleOf3And5(int numberToCheck)//I am trying to write an 'and' statement that would include the modulus operator for both 3 and 5.
+        {
+            return numberToCheck % 5 == 0 && numberToCheck % 3 == 0;
+        }
+
     }
+
 }
--- FuzzBuzz.Tests/FizzBuzzerTest.cs
+++ FuzzBuzz.Tests/FizzBuzzerTest.cs
@@ -19,6 +19,102 @@ namespace FizzBuzz.Tests
 
             Assert.That(result, Is.EqualTo("1"));
         }
+
+        [Test]
+        public void Get_Returns_2_for_2()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(2);
+
+            Assert.That(result, Is.EqualTo("2"));
+        }
+
+        [Test]
+        public void Get_Returns_Fizz_for_3()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(3);
+
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void Get_Returns_Fizz_for_Mul_3()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(3);
+
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void Get_Returns_4_for_4()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(4);
+
+            Assert.That(result, Is.EqualTo("4"));
+        }
+
+        [Test]
+        public void Get_Returns_Buzz_for_5()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(5);
+
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+
+
+        [Test]
+        public void Get_Returns_Buzz_for_Mul_5()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(10);
+
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        public void Get_Returns_Fizz_Buzz_for_Mul_3_and_5()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(30);
+
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+
+        [Test]
+        public void Get_Returns_Buzz_for_7()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(7);
+
+            Assert.That(result, Is.EqualTo("7"));
+        }
+
+
+        [Test]
+        public void Get_Returns_15_for_15()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(15);
+
+            Assert.That(result, Is.EqualTo("15"));
+        }
+
+
+
     }
+
 
 }
